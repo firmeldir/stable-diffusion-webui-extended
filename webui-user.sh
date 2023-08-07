e7209bf098a6effb9f72f3e699e86ed94967ee4f
vladhanin <vhanin@tango.me>
2023-08-07 21:53:34 +0200

    add model downloading

--- webui-user.sh
+++ webui-user.sh
@@ -12,6 +12,8 @@
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
 #export COMMANDLINE_ARGS=""
 
+export COMMANDLINE_ARGS="--api --skip-torch-cuda-test"
+
 # python3 executable
 #python_cmd="python3"
 
